d613b0901896139926bb7ce405716a955ee781a7
DavidButra <david.butra23@gmail.com>
2016-05-24 18:04:28 +0200

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,8 @@
+import caffe
+import os
+
+model_file = '../examples/mnist/lenet_train_test.prototxt'
+petrained_file = '../examples/mnist/lenet_iter_1000.caffemodel'
+net = caffe.Classifier(model_file, petrained_file, image_dims=(28,28), raw_scale=255)
+score = net.predict([caffe.io.load_image('cinco.xcf', color=False)], oversample=False)
+print score
