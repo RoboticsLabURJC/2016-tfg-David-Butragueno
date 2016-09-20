1875b69c65f36f1bb0712f44918dd28588533b6c
DavidButra <david.butra23@gmail.com>
2016-09-20 17:31:36 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -3,6 +3,7 @@ import os
 
 model_file = '../examples/mnist/lenet_train_test.prototxt'
 petrained_file = '../examples/mnist/lenet_iter_1000.caffemodel'
+
 net = caffe.Classifier(model_file, petrained_file, image_dims=(28,28), raw_scale=255)
-score = net.predict([caffe.io.load_image('cinco.xcf', color=False)], oversample=False)
+score = net.predict([caffe.io.load_image('Cinco.bmp', color=False)], oversample=False)
 print score
